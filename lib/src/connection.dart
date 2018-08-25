d2bffa1ead1a31de33a687e398f0517d32ddbd9b
Tobe O <thosakwe@gmail.com>
2018-08-25 16:37:54 -0400

    Basic CassandraConnection class

--- lib/src/connection.dart
+++ lib/src/connection.dart
@@ -1,1 +1,34 @@
-class CassandraConnection {}
+import 'dart:async';
+import 'dart:io';
+import 'binary_reader.dart';
+import 'frame.dart';
+
+class CassandraConnection {
+  final host;
+  final int port;
+  final String username;
+  final String password;
+  final String keyspaceName;
+  final bool useSsl;
+
+  Socket _socket;
+
+  CassandraConnection(this.host, this.port,
+      {this.username, this.password, this.keyspaceName, this.useSsl: false});
+
+  Future open() async {
+    if (_socket != null) {
+      throw new StateError('The connection is already open.');
+    } else {
+      if (useSsl == true) {
+        _socket = await SecureSocket.connect(host, port);
+      } else {
+        _socket = await Socket.connect(host, port);
+      }
+    }
+  }
+
+  Future close() async {
+    await _socket.close();
+  }
+}
